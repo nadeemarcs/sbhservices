af74f0c0b75f8fd1845d484c919e44f13dd2279d
nadeemarcs <61172555+nadeemarcs@users.noreply.github.com>
2023-02-25 17:47:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const options = {
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
+  family: 4, // Use IPv4, skip trying IPv6
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  serverApi: ServerApiVersion.v1
 };
 
 //mongoose.connect(uri, options);
