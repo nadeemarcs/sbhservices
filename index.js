f976886600da607ca99cd2ede1917e2b30ae2b8d
nadeemarcs <61172555+nadeemarcs@users.noreply.github.com>
2023-02-26 22:09:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  //  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://sribalajihospital.vercel.app');
+  
+  
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
