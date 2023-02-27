3d4ea8f300387cddeca7b3ab5c6a1e5eeadee329
nadeemarcs <61172555+nadeemarcs@users.noreply.github.com>
2023-02-27 13:08:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.options('*', cors());
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT, PATCH, DELETE");
   next();
 }); 
 
